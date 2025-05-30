7dd65f393c612346dda47dea1d355bd15f0117a0
DREL10000 <variabledev1@gmail.com>
2025-05-30 22:16:46 +0200

    completed exercises

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,49 @@
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+
+      <Button
+        title="small rounded-sm"
+        styles="px-2 py-1 text-sm rounded-sm text-black bg-white mx-2"
+      />
+      <Button
+        title="small rounded-md"
+        styles="px-2 py-1 text-sm rounded-md text-black bg-white mx-2"
+      />
+      <Button
+        title="small rounded-full"
+        styles="px-2 py-1 text-sm rounded-full text-black bg-white mx-2"
+      />
+      <Button
+        title="medium rounded-sm"
+        styles="px-4 py-2 text-base rounded-sm text-black bg-white mx-2"
+      />
+      <Button
+        title="medium rounded-md"
+        styles="px-4 py-2 text-base rounded-md text-black bg-white mx-2"
+      />
+      <Button
+        title="medium rounded-full"
+        styles="px-4 py-2 text-base rounded-full text-black bg-white mx-2"
+      />
+
+      <Button
+        title="Large rounded-sm"
+        styles="px-6 py-3 text-lg rounded-sm text-black bg-white mx-2"
+      />
+      <Button
+        title="Large rounded-md"
+        styles="px-6 py-3 text-lg rounded-md text-black bg-white mx-2"
+      />
+      <Button
+        title="Large rounded-full"
+        styles="px-6 py-3 text-lg rounded-full text-black bg-white mx-2"
+      />
     </div>
   );
 };
